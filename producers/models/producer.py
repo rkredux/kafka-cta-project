d52f9c343b0aa6fc2106b44c7edb6405f755e375
rkumar <rkredux@gmail.com>
2019-11-27 20:55:47 -0500

    Producer Configurations

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -5,6 +5,7 @@ import time
 from confluent_kafka import avro
 from confluent_kafka.admin import AdminClient, NewTopic, NewPartitions, ConfigResource, ConfigSource
 from confluent_kafka.avro import AvroProducer
+import sleep 
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,10 @@ class Producer:
     def __init__(self,producer_id,topic_name,key_schema,value_schema=None,num_partitions=1,num_replicas=1):
 
         """Initializes a Producer object with basic settings"""
+        #TODO set the client id in a better way
+        #TODO #Async vs Sync Writes and let's read this documentation more thoroughly to understand the difference. 
+        #https://docs.confluent.io/4.0.0/clients/producer.html
+        #Finally let's test this producer before committing the changes
         self.id = producer_id
         self.topic_name = topic_name
         self.key_schema = key_schema
@@ -38,25 +43,33 @@ class Producer:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        #Configure the AvroProducer
+        self.avro_producer = AvroProducer({
+            'bootstrap.servers': self.broker_properties["kafka"], 
+            'schema.registry.url': self.broker_properties["schema_properties"]}, 
+            self.key_schema, 
+            self.value_schema)
+
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        
-        admin_client = AdminClient({"bootstrap.servers": "localhost:9092"})
-        topic_list = []
-        topic_list.append(NewTopic("example_topic", 1, 1))
-        admin_client.create_topics(topic_list)
 
-        logger.info("topic creation kafka integration incomplete - skipping")
+        admin_client = AdminClient({"bootstrap.servers": "localhost:9092"})
+        topic_list = [NewTopic(self.topic_name,self.num_partitions,self.num_replicas)]
+        fs = admin_client.create_topics(topic_list)
+        sleep(10)
+        for topic, f in fs.items(): 
+            try: 
+                f.result()
+                print("Topic {} created".format(topic))
+            except Exception as e: 
+                logger.info("Failed to create topic {}: {}".format(topic, e))
+
 
     def time_millis(self):
         return int(round(time.time() * 1000))
 
+    #TODO What is this and where is it going to be used?
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
         #
@@ -65,7 +78,3 @@ class Producer:
         #
         #
         logger.info("producer close incomplete - skipping")
-
-    def time_millis(self):
-        """Use this function to get the key for Kafka Events"""
-        return int(round(time.time() * 1000))
